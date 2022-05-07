f4fba6c3140a16a30662017bd6f68256aec84a47
agamyo168 <70448818+agamyo168@users.noreply.github.com>
2022-05-07 07:23:08 +0200

    .

--- src/pages/29_UpdateProducts.js
+++ src/pages/29_UpdateProducts.js
@@ -1,13 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-<<<<<<< HEAD
-import OrangeGradientButton from "../Components/Buttons/OrangeGradientButton";
-import { CustomAppBar } from "../Components/MainLayout/AppBar/CustomAppBar";
-import CompanyList from "../Components/ProductList/CompanyList";
-import ProductList from "../Components/ProductList/ProductList";
-import UpdateRoundedIcon from "@mui/icons-material/UpdateRounded";
-=======
 import LoadingSpinner from "../Components/Loaders/LoadingSpinner";
 import { CustomAppBar } from "../Components/MainLayout/AppBar/CustomAppBar";
 import CompanyList from "../Components/ProductList/CompanyList";
@@ -18,7 +11,6 @@ import {
 } from "../services/update";
 import { convertDateToString } from "../functions/convertDateToString";
 import OrangeGradientButton from "../Components/Buttons/OrangeGradientButton";
->>>>>>> 961540a9bb5d4da20ce9d16a8d57a659ce2bd010
 
 export const UpdateProducts = () => {
   const { data, error, isLoading } = useGetLastUpdateInfoQuery();
@@ -28,13 +20,9 @@ export const UpdateProducts = () => {
   const [updateError, setUpdateError] = useState(null);
 
   const textContainer = useSelector((state) => state.language.textContainer);
-<<<<<<< HEAD
-  const pageDictionary = {
-=======
   const language = useSelector((state) => state.language.language);
 
-  const pageDictionry = {
->>>>>>> 961540a9bb5d4da20ce9d16a8d57a659ce2bd010
+  const pageDictionary = {
     updateProductsList: textContainer.updateProductsList,
     lastUpdateDone: "اخر تحديث تم",
     in: "في",
@@ -63,40 +51,6 @@ export const UpdateProducts = () => {
       showLabel
       label={pageDictionary.updateProductsList}
     >
-<<<<<<< HEAD
-      <div style={{ margin: "0px 14.6px" }}>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            paddingBottom: "5px",
-          }}
-        >
-          <Typography variant="S18W700C000000">
-            {pageDictionary.lastUpdate}
-          </Typography>
-          <Typography variant="S16W400C65676b">
-            {pageDictionary.completeSuccess}
-          </Typography>
-        </Box>
-        <ProductList />
-        <br></br>
-        <CompanyList />
-
-        <OrangeGradientButton
-          color="red"
-          sx={{
-            width: "100%",
-            marginTop: "275px", //CHANGE IT AFTER REMOVING THE DOUBLE APPBAR
-          }}
-        >
-          <UpdateRoundedIcon sx={{ fontSize: "28px", marginRight: "6.5px" }} />
-          <Typography variant="S18W700Cffffff">
-            {pageDictionary.updateProducts}
-          </Typography>
-        </OrangeGradientButton>
-      </div>
-=======
       {isLoading ? (
         <LoadingSpinner />
       ) : error || updateError ? (
@@ -113,17 +67,17 @@ export const UpdateProducts = () => {
             }}
           >
             <Typography variant="S18W700C000000">
-              {`${pageDictionry.lastUpdateDone} ${
-                data.automatic ? pageDictionry.auto : pageDictionry.manual
-              } ${pageDictionry.in} ${convertDateToString(
+              {`${pageDictionary.lastUpdateDone} ${
+                data.automatic ? pageDictionary.auto : pageDictionary.manual
+              } ${pageDictionary.in} ${convertDateToString(
                 data.date,
                 language
               )}`}
             </Typography>
             <Typography variant="S16W400C65676b">
               {data.failed
-                ? pageDictionry.updateFailed
-                : pageDictionry.completeSuccess}
+                ? pageDictionary.updateFailed
+                : pageDictionary.completeSuccess}
             </Typography>
           </Box>
           <ProductList list={data.phones} />
@@ -135,7 +89,6 @@ export const UpdateProducts = () => {
           />
         </div>
       )}
->>>>>>> 961540a9bb5d4da20ce9d16a8d57a659ce2bd010
     </CustomAppBar>
   );
 };
