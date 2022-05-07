49a3b78d2e73dd60f0d945c25d6f5894be1cd0bf
agamyo168 <70448818+agamyo168@users.noreply.github.com>
2022-05-07 07:19:25 +0200

    added button in UpdateProducts

--- src/pages/29_UpdateProducts.js
+++ src/pages/29_UpdateProducts.js
@@ -1,23 +1,27 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
 import { useSelector } from "react-redux";
+import OrangeGradientButton from "../Components/Buttons/OrangeGradientButton";
 import { CustomAppBar } from "../Components/MainLayout/AppBar/CustomAppBar";
 import CompanyList from "../Components/ProductList/CompanyList";
 import ProductList from "../Components/ProductList/ProductList";
+import UpdateRoundedIcon from "@mui/icons-material/UpdateRounded";
 
 export const UpdateProducts = () => {
   const textContainer = useSelector((state) => state.language.textContainer);
-  const pageDictionry = {
+  const pageDictionary = {
     updateProductsList: textContainer.updateProductsList,
     lastUpdate: "اخر تحديث تم يدوياً/ذاتياً في (تاريخ)",
     completeSuccess: "اكتمل بنجاح",
     updateFailed: "فشل التحديث",
+    updateProducts: textContainer.updateProducts,
+    updating: textContainer.updating,
   };
   return (
     <CustomAppBar
       showBackBtn
       showLabel
-      label={pageDictionry.updateProductsList}
+      label={pageDictionary.updateProductsList}
     >
       <div style={{ margin: "0px 14.6px" }}>
         <Box
@@ -28,15 +32,28 @@ export const UpdateProducts = () => {
           }}
         >
           <Typography variant="S18W700C000000">
-            {pageDictionry.lastUpdate}
+            {pageDictionary.lastUpdate}
           </Typography>
           <Typography variant="S16W400C65676b">
-            {pageDictionry.completeSuccess}
+            {pageDictionary.completeSuccess}
           </Typography>
         </Box>
         <ProductList />
         <br></br>
         <CompanyList />
+
+        <OrangeGradientButton
+          color="red"
+          sx={{
+            width: "100%",
+            marginTop: "275px", //CHANGE IT AFTER REMOVING THE DOUBLE APPBAR
+          }}
+        >
+          <UpdateRoundedIcon sx={{ fontSize: "28px", marginRight: "6.5px" }} />
+          <Typography variant="S18W700Cffffff">
+            {pageDictionary.updateProducts}
+          </Typography>
+        </OrangeGradientButton>
       </div>
     </CustomAppBar>
   );
