6f13a2f2a7519ef1fe2af5e7bf0e65421d81f4b5
FadyAhmed <fady.999666@gmail.com>
2022-05-08 16:28:09 +0200

    signOut dialog handled

--- src/Components/Dialogs/SignoutDialog.js
+++ src/Components/Dialogs/SignoutDialog.js
@@ -5,13 +5,41 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import BasicButton from "../Buttons/BasicButton";
 import { DialogTemplate } from "./DialogTemplate";
+import { logout } from "../../Authentication/auth";
+import { useAppDispatch } from "../../store/hooks";
+import { useLogoutFromAllDevicesMutation } from "../../services/users";
+import { authActions } from "../../store/authSlice";
+
 export const SignoutDialog = ({ handleClose }) => {
+  const dispatch = useAppDispatch();
+
   const [checked, setChecked] = useState(false);
+  const [signOutError, setSignOutError] = useState(null);
+  const [logoutFromAllDevices] = useLogoutFromAllDevicesMutation();
+
   const theme = useTheme();
   const textContainer = useSelector((state) => state.language.textContainer);
   const handleChecked = () => {
     setChecked(!checked);
   };
+
+  const logOutFromAllDevices = async () => {
+    try {
+      await logoutFromAllDevices();
+      await setSignOutError(logout());
+      dispatch(authActions.logout());
+      handleClose();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const signout = async () => {
+    await setSignOutError(logout());
+    dispatch(authActions.logout());
+    handleClose();
+  };
+
   return (
     <React.Fragment>
       <DialogTemplate
@@ -61,7 +89,10 @@ export const SignoutDialog = ({ handleClose }) => {
           }}
         >
           <BasicButton>
-            <BasicButton sx={{ marginLeft: "22px" }}>
+            <BasicButton
+              onClick={() => (checked ? logOutFromAllDevices() : signout())}
+              sx={{ marginLeft: "22px" }}
+            >
               <Typography variant="S16W800CE41D1D">
                 {textContainer.logOut}
               </Typography>
