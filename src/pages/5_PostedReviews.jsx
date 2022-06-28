32de88a1009e9f09dc4079a7e4dd56e9bcb30d7c
FadyAhmed <fady.999666@gmail.com>
2022-06-29 01:08:31 +0200

    filter tabbar added

--- src/pages/5_PostedReviews.jsx
+++ src/pages/5_PostedReviews.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useTheme } from "@emotion/react";
+import { Fragment, useState } from "react";
 import { FixedGrid } from "../Components/Grid/FixedGrid";
 import { CustomAppBar } from "../Components/MainLayout/AppBar/CustomAppBar";
 import { FilterTabbar } from "../Components/Tabbar/FilterTabbar";
@@ -8,6 +9,8 @@ import { PostedPhoneReviews } from "./5_PostedPhoneReviews";
 export function PostedReviews() {
   const [filter, setFilter] = useState(0);
 
+  const isMobile = useTheme().isMobile;
+
   return (
     <CustomAppBar
       showLabel
@@ -16,6 +19,15 @@ export function PostedReviews() {
       tabBar={<FilterTabbar value={filter} setValue={setFilter} />}
     >
       <FixedGrid>
+        {!isMobile && (
+          <div style={{ marginTop: "12px" }}>
+            <FilterTabbar
+              withBackground={false}
+              value={filter}
+              setValue={setFilter}
+            />
+          </div>
+        )}
         {filter === 0 ? <PostedPhoneReviews /> : <PostedCompanyReviews />}
       </FixedGrid>
     </CustomAppBar>
