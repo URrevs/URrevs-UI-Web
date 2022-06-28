9b2a33a8959edfc4789a040d3666e2823a18ed73
agamyo168 <70448818+agamyo168@users.noreply.github.com>
2022-06-28 22:06:57 +0200

    modified star error

--- src/Components/Form/FormikStar.js
+++ src/Components/Form/FormikStar.js
@@ -1,5 +1,6 @@
 import { Field } from "formik";
 import React from "react";
+import { useAppSelector } from "../../store/hooks";
 import StarRating from "./StarRating";
 
 const FormikStar = ({
@@ -11,6 +12,7 @@ const FormikStar = ({
   starDimension = "35px",
   starSpacing = "5px",
 }) => {
+  const textContainer = useAppSelector((state) => state.language.textContainer);
   return (
     <>
       <Field name={fieldName}>
@@ -43,7 +45,7 @@ const FormikStar = ({
                     // marginLeft: "14px",
                   }}
                 >
-                  {meta.error}
+                  {textContainer.starRatingMissingField}
                 </p>
               )}
             </div>
