1627699162f90eb6466b533a729ee835ce54ec60
agamyo168 <70448818+agamyo168@users.noreply.github.com>
2022-07-03 16:57:54 +0200

    snackbar error handling done

--- src/store/errorSnackbarHandle.ts
+++ src/store/errorSnackbarHandle.ts
@@ -3,20 +3,85 @@ import type { MiddlewareAPI, Middleware } from "@reduxjs/toolkit";
 import { snackbarActions } from "./uiSnackbarSlice";
 import { RootState } from "./store";
 import Dictionary from "../Dictionary";
+import { createSecureServer } from "http2";
+
+interface Error {
+  message: string;
+  isError: boolean;
+}
 
 const correspondingErrorMessage = (
   serverMessage: string,
   textContainer: Dictionary
-): string => {
+): Error => {
+  let error: Error = {
+    message: "",
+    isError: true,
+  };
+
   switch (serverMessage) {
     case "invalid token":
-      return textContainer.invalidToken;
-    case "you are not an admin":
-      return textContainer.youAreNotAdmin;
-    default:
-      return textContainer.internalServerError;
+      error.message = textContainer.invalidToken;
       break;
+    case "process failed":
+      error.message = textContainer.processFailed;
+      break;
+    case "no update operations yet":
+      error.message = textContainer.noUpdateOperationsYet;
+      break;
+    case "you are not an admin":
+      error.message = textContainer.youAreNotAdmin;
+      break;
+    case "there is a running update operation right now":
+      error.message = textContainer.thereIsARunningUpdateOperationRightNow;
+      break;
+    case "bad request":
+      error.message = textContainer.badRequest;
+      break;
+    //TO DO : WRITE TRANSLATIONS AND FIND MORE ERRORS
+    // case "track internal server error":
+    //   error.message = textContainer.trackInternalServerError;
+    //   break;
+    // case "already liked":
+    //   error.message = textContainer.alreadyLiked;
+    //   break;
+    // case "already unliked":
+    //   error.message = textContainer.alreadyUnliked;
+    //   break;
+    // case "no likes":
+    //   error.message = textContainer.noLikes;
+    //   break;
+    // case "not found":
+    //   error.message = textContainer.notFound;
+    //   break;
+    // case "not owned":
+    //   error.message = textContainer.notOwned;
+    //   break;
+    // case "not yet":
+    //   error.message = textContainer.notYet;
+    //   break;
+    // case "not accepted":
+    //   error.message = textContainer.notAccepted;
+    //   break;
+    // case "already reviewed":
+    //   error.message = textContainer.alreadyReviewed;
+    //   break;
+    // case "invalid referral code":
+    //   error.message = textContainer.invalidReferralCode;
+    //   break;
+    // case "already reported":
+    //   error.message = textContainer.alreadyReported;
+    //   break;
+    // case "blocked":
+    //   error.message = textContainer.blocked;
+    //   break;
+    // case "already accepted":
+    //   error.message = textContainer.alreadyAccepted;
+    //   break;
+    default:
+      error.message = textContainer.internalServerError;
   }
+  return error;
 };
 
 export const snackbarErrorHandle: Middleware =
@@ -30,16 +95,17 @@ export const snackbarErrorHandle: Middleware =
     if (isRejectedWithValue(action)) {
       const serverMessage: string = action.payload.data.status;
 
-      const message: string = correspondingErrorMessage(
+      const message: Error = correspondingErrorMessage(
         serverMessage,
         textContainer
       );
-
-      dispatch(
-        snackbarActions.showSnackbar({
-          message: message,
-        })
-      );
+      if (message.isError) {
+        dispatch(
+          snackbarActions.showSnackbar({
+            message: message.message,
+          })
+        );
+      }
       console.log(action);
     }
 
