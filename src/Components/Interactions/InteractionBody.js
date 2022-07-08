57958d190c68627c7467b445f83b235d6cf0e79a
agamyo168 <70448818+agamyo168@users.noreply.github.com>
2022-07-08 23:15:27 +0200

    added report on hover but still needs better UI/UX

--- src/Components/Interactions/InteractionBody.js
+++ src/Components/Interactions/InteractionBody.js
@@ -1,8 +1,9 @@
-import { Avatar, Box } from "@mui/material";
+import { Avatar, Box, Grid, IconButton, Stack } from "@mui/material";
 import React from "react";
 import { InteractionCard } from "./InteractionCard";
 import { Link } from "react-router-dom";
 import ROUTES_NAMES from "../../RoutesNames";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 export const InteractionBody = ({
   text,
@@ -15,10 +16,12 @@ export const InteractionBody = ({
   userId,
   userName,
 }) => {
+  const [showReportMenu, setShowReportMenu] = React.useState(true);
   return (
     <div
       style={{
         whiteSpace: "pre-line",
+        wordBreak: "break-all",
       }}
     >
       <Box sx={{ display: "flex" }}>
@@ -28,15 +31,42 @@ export const InteractionBody = ({
             sx={{ marginRight: "6px", height: avatarSize, width: avatarSize }}
           ></Avatar>
         </Link>
-        <Box sx={{ display: "flex", flexDirection: "column" }}>
-          <InteractionCard
-            userName={userName}
-            userId={userId}
-            text={text}
-            ownedAt={ownedAt}
-            likes={likes}
-            renderIcon={renderIcon}
-          ></InteractionCard>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              width: "100%",
+            }}
+            onMouseEnter={() => {
+              setShowReportMenu(true);
+            }}
+            onMouseLeave={() => {
+              setShowReportMenu(false);
+            }}
+          >
+            <InteractionCard
+              userName={userName}
+              userId={userId}
+              text={text}
+              ownedAt={ownedAt}
+              likes={likes}
+              renderIcon={renderIcon}
+            />
+            <div>
+              {showReportMenu && (
+                // <div>...</div>
+                <IconButton>
+                  <MoreHorizIcon />
+                </IconButton>
+              )}
+            </div>
+          </div>
           <Box
             sx={{
               display: "flex",
