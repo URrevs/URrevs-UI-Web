87c54e42446f8892a2ce36270600c2488613e4f3
FadyAhmed <fady.999666@gmail.com>
2022-07-15 13:30:26 +0200

    loading skeleton added

--- src/pages/17_PhoneQuestionFullScreen.jsx
+++ src/pages/17_PhoneQuestionFullScreen.jsx
@@ -5,6 +5,7 @@ import { useCallback, useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { AlonePostsGrid } from "../Components/Grid/AlonePostsGrid";
 import { FixedGrid } from "../Components/Grid/FixedGrid";
+import LoadingReviewSkeleton from "../Components/Loaders/LoadingReviewSkeleton";
 import { CustomAppBar } from "../Components/MainLayout/AppBar/CustomAppBar";
 import { PostingField } from "../Components/PostingComponents/PostingField";
 import PhoneQuestion from "../Components/ReviewCard/phoneQuestion";
