526bde726766dc3be8efee55318dda5975bce693
agamyo168 <70448818+agamyo168@users.noreply.github.com>
2022-07-17 03:53:59 +0200

    added new linkedin icon

--- src/Components/Icons/LinkedIn.js
+++ src/Components/Icons/LinkedIn.js
@@ -1,5 +1,12 @@
 const LinkedIn = () => {
-  return <img alt="linkedIn logo" src="/images/linkedLogo.png" />;
+  return (
+    <img
+      alt="linkedIn logo"
+      src="/images/linkedLogo.png"
+      height="44px"
+      width="45px"
+    />
+  );
 };
 
 export default LinkedIn;
